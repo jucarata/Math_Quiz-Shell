19b376aa5444e7ec7374822df2e253e22b85e484
JCamiloRamirezTabares <juancaram21@gmail.com>
2025-05-12 13:03:09 -0500

    feat: added multiply operator function

--- operators/multiplication.sh
+++ operators/multiplication.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
 function multiplication {
-    typeset -i num1 num2
+    typeset -i num1 num2 attempts real_res res
     num1=$1
-    num2=$2
+    num2=$(( $2 % 10 + 1 )) 
+    real_res=$((num1 * num2))
 
-    echo "Multiplicacion de $num1 * $num2"
+    attempts=0
+
+    while (( attempts < 3 ))
+    do
+        echo "¿Cuánto es: $num1 x $num2 ?"
+        read -p "Respuesta: " res
+
+        ((attempts++))
+
+        if (( res == real_res )) then
+            echo "¡Felicitaciones! Esa es la respuesta correcta."
+            break
+        fi
+
+        if ((attempts != 3)) then
+            echo "Ups, no es correcto. ¡Intenta de nuevo!"
+        fi
+    done
+
+    if (( attempts == 3 )) && (( res != real_res )); then
+        echo "Oh, lo siento. Te has quedado sin intentos. La respuesta correcta era $real_res."
+    fi
 }
